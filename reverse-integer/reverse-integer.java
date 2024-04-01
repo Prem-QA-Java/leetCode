080e00ad01a44b5c4ba68cb8b0e937db717e6388
Prem Sai <123071774+Prem-QA-Java@users.noreply.github.com>
2024-04-01 16:15:38 +0530

    Time: 10 ms, Memory: 42 MB - LeetHub

---
+++ reverse-integer/reverse-integer.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reverse(int x) {
+        long l=0;
+        String str=String.valueOf(x);
+        String s="";
+        String symbol="";
+        for(int i=0;i<str.length();i++){
+            if(!Character.isDigit(str.charAt(0))&&i==0){
+                symbol = String.valueOf(str.charAt(0));
+            }else{
+                char n = str.charAt(i);
+                s=n+s;
+            }
+        }
+        l=Long.parseLong(symbol+s);
+        if(l>Integer.MAX_VALUE || l<Integer.MIN_VALUE){
+            return 0;
+        }
+        return (int)l;
+    }
+}
